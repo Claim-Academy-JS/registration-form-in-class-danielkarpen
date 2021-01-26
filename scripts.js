404191d6c23dcfb0fffe3ade3750850f93ff51ce
Daniel <danielkarpen97@gmail.com>
2021-01-26 17:10:24 -0600

    fixed toggle issue for chrome browser

--- scripts.js
+++ scripts.js
@@ -4,16 +4,23 @@
 // when toggleBtn is clicked, hide all inputs except for username and password
 const submitBtn = document.querySelector('button[type="submit"]');
 const toggleBtn = document.querySelector("#toggle-btn");
-const toggledInputs = document.querySelectorAll("input:not(#uname, #password)");
+const toggledInputs = Array.from(document.querySelectorAll("input")).slice(
+  0,
+  3
+);
 
 toggleBtn.addEventListener("click", function () {
-  for (let i = 0; i < toggledInputs.length; i += 1) {
-    toggledInputs[i].classList.add("is-hidden");
-  }
   if (this.innerText === "Already have an account?") {
+    for (let i = 0; i < toggledInputs.length; i += 1) {
+      toggledInputs[i].classList.add("is-hidden");
+    }
     this.innerText = "Need to create an account?";
     submitBtn.innerText = "Login!";
   } else {
+    const hiddenInputs = document.querySelectorAll("input.is-hidden");
+    for (let i = 0; i < hiddenInputs.length; i += 1) {
+      hiddenInputs[i].classList.add("is-visible");
+    }
     this.innerText = "Already have an account?";
     submitBtn.innerText = "Register!";
   }
