fa52c88b03d1cc51925167fe0f00bc34d8798c52
Daniel <danielkarpen97@gmail.com>
2021-01-26 14:49:58 -0600

    add second button grab reference already

--- scripts.js
+++ scripts.js
@@ -1,16 +1,4 @@
-// 'querySelectors' use a string that corresponds with a CSS selector
-const usernameInput = document.querySelector("#uname");
-usernameInput.addEventListener(
-  // what do i listen for
-  "input",
-  // what action do i perform when this event occurs ('change' event)?
-  // wow we can pass a function into another function/object this is known as a callback function
-  () => {
-    console.log("hello! I was changed!");
-  }
-);
+// TODO: when we click already thn all fields should be hidden except username and password
+// 1. Grab a reference the other button that says 'already'
 
-// document is the object provided by DOM API
-//  querySelector is a method it takes a string argument ""
-// that string argument can be any CSS selector
-document.querySelector("#fname");
+const alreadyBtn = document.querySelector("#toggle-btn");
