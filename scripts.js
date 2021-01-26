0b09cd03e085dd2007267431acd089f1d066eb83
Daniel <danielkarpen97@gmail.com>
2021-01-26 16:22:22 -0600

    add queryselector to toggled inputs

--- scripts.js
+++ scripts.js
@@ -4,12 +4,12 @@
 // when toggleBtn is clicked, hide all inputs except for username and password
 const submitBtn = document.querySelector('button[type="submit"]');
 const toggleBtn = document.querySelector("#toggle-btn");
-
 const toggledInputs = document.querySelectorAll("input:not(#uname, #password)");
 
-console.log(submitBtn);
-
 toggleBtn.addEventListener("click", function () {
+  for (let i = 0; i < toggledInputs.length; i += 1) {
+    toggledInputs[i].classList.add("is-hidden");
+  }
   if (this.innerText === "Already have an account?") {
     this.innerText = "Need to create an account?";
     submitBtn.innerText = "Login!";
