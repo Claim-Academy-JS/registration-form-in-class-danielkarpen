2553180fb76f82516a5987be93312ef7e13eedff
Daniel <danielkarpen97@gmail.com>
2021-01-26 15:03:45 -0600

    change toggle on click and select submit button

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 // TODO: when we click already thn all fields should be hidden except username and password
 // 1. Grab a reference the other button that says 'already'
 
+// when toggleBtn is clicked, hide all inputs except for username and password
+const submitBtn = document.querySelector('button[type="submit"]');
 const toggleBtn = document.querySelector("#toggle-btn");
 
-toggleBtn.addEventListener("click", () => {
-  console.log("I was clicked!");
+toggleBtn.addEventListener("click", function () {
+  this.textContent = "Need to create an account?";
 });
