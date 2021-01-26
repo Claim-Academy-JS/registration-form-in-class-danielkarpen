1b3a1b059341b58b87c84366947dbd8c0dfde726
Daniel <danielkarpen97@gmail.com>
2021-01-26 14:55:28 -0600

    addevent listener listen for clicks toggleBtn

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 // TODO: when we click already thn all fields should be hidden except username and password
 // 1. Grab a reference the other button that says 'already'
 
-const alreadyBtn = document.querySelector("#toggle-btn");
+const toggleBtn = document.querySelector("#toggle-btn");
+
+toggleBtn.addEventListener("click", () => {
+  console.log("I was clicked!");
+});
